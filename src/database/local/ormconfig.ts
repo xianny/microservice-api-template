35616d321b977dc23c229144c32f4c5400dba6f3
xianny <xianny@gmail.com>
2021-04-16 13:10:46 -0700

    remove nonexistent files

--- src/database/local/ormconfig.ts
+++ src/database/local/ormconfig.ts
@@ -3,16 +3,9 @@ import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConne
 
 import { config as appConfig } from '../../config';
 
-import { DexName } from './entities/Dex';
-import { QuoteEntity } from './entities/Quote';
-import { QuotePriceComparisonEntity } from './entities/QuotePriceComparison';
-
-const entities: any[] = [DexName, QuoteEntity, QuotePriceComparisonEntity];
-
 const config: PostgresConnectionOptions = {
     name: 'default',
     type: 'postgres',
-    entities,
     migrations: ['./lib/src/database/local/migrations/*.js'],
     synchronize: process.env.SYNC_DB === 'true', // for staging
     logging: true,
